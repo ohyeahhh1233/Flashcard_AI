3da59d61ba2ee94b76824219163d36dd3543034b
ohyeahhh1233 <110427443+ohyeahhh1233@users.noreply.github.com>
2025-06-01 17:54:39 -0700

    Update FlashCardManager.java
    finished the manager now i got to figure out the app

--- FlashCardManager.java
+++ FlashCardManager.java
@@ -10,7 +10,7 @@ public class FlashCardManager {
         String knowledge = input.nextLine();
         System.out.println("Enter Answer:");
         String WhatYaGot = input.nextLine();
-        cards.add(new Flashcard(knowledge, WhatYaGot));
+        cards.add(new Flashcards(knowledge, WhatYaGot));
         System.out.println("Flashcard added. ((\n");
     }
     private void reviews(){
@@ -30,12 +30,30 @@ public class FlashCardManager {
         }
     }
     private void stats(){
-        for (Flashcard card : cards){
+        for (Flashcards card : cards){
             System.out.println("\"" + card.getQuestion() "\" > " + card.YourRight() + " correct / " + card.YourWrong + " wrong");
         }
     }
     public void SaveFile(Strong filename) throws IOException{
-        
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filename))){
+            for (Flashcards card : cards){
+                writer.println(card);
+            }
+        }
+    }
+    public void LoadFile(String filename) throws IOException{
+        cards.clear();
+        File file = new File(filename);
+        if (!file.exists()) return;
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String line;
+            while((line = reader.readLine()) != null){
+                String[] parts = line.split(",");
+                Flashcards card = new Flashcards(parts[0], parts[1]);
+                card.YourRight();
+                cards.add(card);
+            }
+        }
     }
 }
 
